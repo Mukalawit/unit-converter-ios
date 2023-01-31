c9e1816c2acb920a7fc49fb0c04f8560037f37f9
mukalawit <mgstn15@gmail.com>
2023-01-31 15:08:29 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Anthony Mugasa Jr on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
